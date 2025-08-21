4233868a895646216dcd7b194bb591de1217b12a
Abhishek Singh <xing12505@gmail.com>
2025-08-22 02:06:53 +0530

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,24 @@
+function goToStory() {
+  window.location.href = "story.html";
+}
+
+function goToGallery() {
+  window.location.href = "gallery.html";
+}
+
+function goToWishes() {
+  window.location.href = "wishes.html";
+}
+
+function goToGame() {
+  window.location.href = "game.html";
+}
+
+function rightAnswer() {
+  alert("Correct! ❤️ It's always Chiku 😘");
+  window.location.href = "treasure.html";
+}
+
+function wrongAnswer() {
+  alert("Hehe, wrong 😅 Try again!");
+}
